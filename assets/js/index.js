f2c1bbcf6c506c92bd1adf703a9cdb3fe9d667f7
jovyzabel <jovymonka7@gmail.com>
2024-07-29 17:57:04 +0100

    font family import

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,15 @@
 let slideIndex = 0;
 showSlides();
 
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -10,5 +19,6 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 7000); 
 }
+
